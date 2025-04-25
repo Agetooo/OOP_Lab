ec707338dc35c02f2f747c20d7a97105f4d34c9b
Agetooo <kiwinno181105@gmail.com>
2025-04-25 20:50:17 +0700

    Lab03

---
+++ Lab03/AimsProject/src/hust/soict/hedspi/aims/media/DigitalVideoDisc.java
@@ -0,0 +1,80 @@
+package hust.soict.hedspi.aims.media;
+
+public class DigitalVideoDisc extends Disc implements Playable{
+	private static int nbDigitalVideoDiscs = 0;
+	private String director;
+	private int length;
+	
+	public String getDirector() {
+		return director;
+	}
+
+	public int getLength() {
+		return length;
+	}
+	public DigitalVideoDisc(int id, String title, String category, float cost, int length, String director) {
+        super(id, title, category, cost, length, director);
+    }
+
+	 public DigitalVideoDisc(String title, String category, String director, int length, double cost) {
+	        super(title, category, cost);
+	        this.director = director;
+	        this.length = length;
+	        assignID();
+	    }
+	 public DigitalVideoDisc(int id, String title, String category, float cost) {
+	        super(id, title, category, cost);
+	    }
+
+	public DigitalVideoDisc(String title, String category, double cost) {
+		super(title,category,cost);
+		assignID();
+	}
+
+	public DigitalVideoDisc(String title, String category, String director, double cost) {
+		super(title,category,cost);
+		this.director = director;
+		assignID();
+	}
+
+	public DigitalVideoDisc(String title) {
+		super(title);
+		assignID();
+	}
+	public void assignID() {
+		nbDigitalVideoDiscs++;
+	   this.id= nbDigitalVideoDiscs +100;
+	}
+	
+	 public static void decreaseNbDigitalVideoDiscs() {
+	        if (nbDigitalVideoDiscs > 0) {
+	            nbDigitalVideoDiscs--;
+	        }
+	    }
+	 public static int getNbDigitalVideoDiscs() {
+	        return nbDigitalVideoDiscs;
+	    }
+	 public static void displayNumber() {
+	        System.out.println("\nNumber of orders now is: " + getNbDigitalVideoDiscs());
+	    }
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	@Override
+	public String toString() {
+	    return " ID - " + id + " - DVD - " + title + " - " + category + " - " + director + " - " + length + ": " + cost + " $";
+	}
+
+	public boolean isMatch(String title) {
+	    return this.title != null && this.title.equalsIgnoreCase(title);
+	}
+	@Override
+    public void play() {
+        System.out.println("Playing DVD: " + this.getTitle());
+        System.out.println("DVD length: " + this.getLength());
+    }
+}
+
+	
---
+++ Lab03/OtherProjects/src/hust/soict/hedspi/garbage/NoGarbage.java
@@ -0,0 +1,23 @@
+package hust.soict.hedspi.garbage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class NoGarbage {
+    public static void main(String[] args) throws IOException {
+        String filename = "test.exe"; 
+        byte[] inputBytes = Files.readAllBytes(Paths.get(filename));
+
+        long startTime = System.currentTimeMillis();
+
+        StringBuffer outputStringBuffer = new StringBuffer();
+        for (byte b : inputBytes) {
+            outputStringBuffer.append((char) b);
+        }
+        String outputString = outputStringBuffer.toString();
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("NoGarbage runtime: " + (endTime - startTime) + "ms");
+    }
+}
